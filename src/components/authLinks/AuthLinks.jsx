d11147f7b90e32b0b09475973d3bc973037321bb
Yeeun Lee <maraisienne.lee@gmail.com>
2023-11-15 18:26:02 +0900

    feat: Auth.js 적용

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import styles from "./authLinks.module.css";
 import Link from "next/link";
+import { signOut } from "next-auth/react";
 
 const AuthLinks = () => {
 	const [open, setOpen] = useState(false);
@@ -17,7 +18,9 @@ const AuthLinks = () => {
 			) : (
 				<>
 					<Link href="/write">Write</Link>
-					<span className={styles.link}>Logout</span>
+					<span className={styles.link} onClick={() => signOut()}>
+						Logout
+					</span>
 				</>
 			)}
 			<div className={styles.burger} onClick={() => setOpen(!open)}>
@@ -31,7 +34,7 @@ const AuthLinks = () => {
 					<Link href="/">About</Link>
 					<Link href="/">Contact</Link>
 					{status === "notAuthenticated" ? (
-						<Link href="/login" >Login</Link>
+						<Link href="/login">Login</Link>
 					) : (
 						<>
 							<Link href="/write">Write</Link>
